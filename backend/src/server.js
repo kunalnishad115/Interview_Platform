dc536b77c6485964daca0705960bf9af5ee9c629
kunalnishad115 <nishadkunal1234567@gmail.com>
2025-12-04 17:39:15 +0530

    little changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
 import path from 'path';
-// import { connect } from 'http2';
 import { connectDB } from './lib/db.js';
-const app = express();
+import cors from 'cors';
+import {serve} from 'inngest/express';
+import { inngest } from './lib/inngest.js';
+import {functions} from './lib/inngest.js';
 
+
+const app = express();
 const __dirname = path.resolve();
+app.use(express.json());
+// app.use(cors()) // devlopment style
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true})) // production style
+
+app.use('/api/inngest',serve({client:inngest, functions}))
+
+
 
 
 app.get('/books',(req,res)=>{
@@ -20,17 +31,6 @@ if(ENV.NODE_ENV=="production"){
     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
   }) 
 }
-
-
-
-
-
-
-
-
-
-
-
 
 const startServer=async()=>{
   try{
